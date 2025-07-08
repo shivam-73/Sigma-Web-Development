34bd6a1b958b5ef0c01d005bb5735aac94629055
shivam <maddheshiyashivam786@gmail.com>
2025-07-08 22:34:03 +0530

    JAVASCRIPT

---
+++ Video43/script.js
@@ -0,0 +1,13 @@
+console.log("Code is running")
+console.log("Code is still running")
+
+var a = prompt("Enter your number: ")
+console.log("Code number" + " " + a + " " + "is running")
+
+var isTrue = confirm("Do you really want to close the application and blast your computer")
+if(isTrue){
+    console.log("Computer is blasting")
+}
+else{
+    console.log("Computer is not blasting")
+}

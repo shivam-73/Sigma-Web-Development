30777ab018cb69bc0389e172b075632694875f0c
shivam <maddheshiyashivam786@gmail.com>
2025-07-10 00:01:16 +0530

    Variables,Data Type

---
+++ Video44/script.js
@@ -0,0 +1,41 @@
+console.log("Hey, This is tutorial 55")
+
+var a = 55
+
+{
+    var a = 10
+    console.log(a)
+}
+
+console.log(a)
+
+let b  = 24
+
+{
+    let b = 21
+    console.log(b)
+}
+console.log(b)
+
+
+let x = "Harry Bhai";
+let y = 22;
+let z = 2.34;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p ,typeof q , typeof r );
+
+let o = {
+    "name": "shivam",
+    "job code" : 3200
+
+} 
+console.log(o);
+o.salary = "100crore";
+console.log(o)
+
+o.salary = "500crore";
+console.log(o)

7fa05583da83ba79f666d9ab91e547d2576fe55c
shivam <maddheshiyashivam786@gmail.com>
2025-07-10 23:14:52 +0530

    Loops

---
+++ Video46/script.js
@@ -0,0 +1,35 @@
+console.log("I am tutorial on loops")
+
+// let a = 1;
+// console.log(a);
+// console.log(a+2);
+// console.log(a+4);
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(i);
+    
+// }
+
+// let obj = {
+//     "name":"harry",
+//     "role" : "pragrammer",
+//     company : "code with harry"
+// } 
+
+// for(const key in obj){
+//     const element = obj[key];
+//     console.log(key,":",element);
+// }
+
+// let i = 10;
+// while(i < 6){
+//     console.log(i);
+//     i++;
+// }
+
+
+// do-while Run atleast one time
+// do {
+//     console.log(i);
+//     i++
+// } while (i<6);

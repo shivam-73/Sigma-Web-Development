452fd62657a4fe0bf2bd1a060989e0e0fa41f417
shivam <maddheshiyashivam786@gmail.com>
2025-07-10 15:38:18 +0530

    if-else

---
+++ Video45/script.js
@@ -0,0 +1,37 @@
+console.log("hello i am a conditional tutorial")
+
+let age = 18;
+// let grace = 2;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age * grace)
+// console.log(age % grace)
+
+if(age != 18){
+    console.log("You are adult")
+}
+else{
+    console.log("You are minor")
+}
+
+//  === Check the data type and value
+// > "3" == 3
+// true
+// > "3" === 3
+// false
+// > !true
+// false
+// > !false
+// true
+// > a = 6
+// 6
+// > b = 7
+// 7
+// > a!=b
+// true
+// > a!=b && b!=a
+// true
+// >

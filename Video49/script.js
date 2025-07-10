58fdc2c614ca0bc39bea16fc3a42f9d409073234
shivam <maddheshiyashivam786@gmail.com>
2025-07-11 00:50:28 +0530

    Strings

---
+++ Video49/script.js
@@ -0,0 +1,36 @@
+console.log("This is string turorials");
+
+let a = "Harry";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log("The length of string is " + a.length)
+
+
+let real_name = "Shivam"
+let friend = "aditya"
+console.log("his name is " + real_name + " and his friend name is "+ friend)
+
+//template literals
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Shivam";
+console.log(b.length);
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(1,4));
+console.log(b.slice(2));
+
+console.log(b.concat(a));
+console.log(b.replace("hi","hlo"));
+// console.log(b[6]);
+// console.log(b.charAt(6));
+console.log(b.startsWith("Shivam"))
+console.log(b.endsWith("Shivam"))
+
+let c ="Rohan"
+console.log(c)

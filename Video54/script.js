61c435b6718719d9aaa90011022c59999cc33a4d
shivam <maddheshiyashivam786@gmail.com>
2025-07-15 00:37:27 +0530

    Selecting

---
+++ Video54/script.js
@@ -0,0 +1,14 @@
+console.log("hello bro");
+
+// let boxes = document.getElementsByClassName("box");
+
+// boxes[2].style.backgroundColor = "red";
+
+document.getElementById("redbox").style.backgroundColor = "red"
+
+    // document.querySelectorAll(".box").forEach(e =>{
+    //     e.style.background = "green";
+    // })
+
+    
+document.getElementsByTagName("div");

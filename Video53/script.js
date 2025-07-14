67c42ec2e66fc0535dea87551890c902fe5d3aef
shivam <maddheshiyashivam786@gmail.com>
2025-07-14 10:57:49 +0530

    DOM(Document object model)

---
+++ Video53/script.js
@@ -0,0 +1,34 @@
+console.log("Hello World")
+
+
+
+/*
+script.js:1 Hello World
+document.body
+document.body.children
+HTMLCollection(3) [div.container, script, script]
+document.body.childNodes[0]
+#text
+document.body.childNodes[1].childNodes
+NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+let cont = document.body.childNodes[1]
+undefined
+cont.firstChild
+#text
+cont.lastChild
+#text
+cont.firstChild
+#text
+cont.firstElementChild
+
+cont.firstElementChild.style.backgroundColor= "yellow"
+'yellow'
+
+Hello World
+let tab = document.body.children[1]
+undefined
+let tab = document.body.children[1]
+undefined
+tab.rows
+HTMLCollection(2) [tr, tr]
+*/

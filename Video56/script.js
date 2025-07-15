b86158c44b2440f3f20dbfac02b9ae5078981653
shivam <maddheshiyashivam786@gmail.com>
2025-07-15 21:27:22 +0530

    Insertion & deletion element using JS

---
+++ Video56/script.js
@@ -0,0 +1,24 @@
+/* 
+document.querySelector(".box")
+<div class=​"box">​ Hey I am a box ​</div>​
+document.querySelector(".box").innerText
+'Hey I am a box'
+document.querySelector(".container").innerHTML
+'\n        <div class="box">\n            Hey I am a box\n        </div>\n    '
+document.querySelector(".container").innerText
+'Hey I am a box'
+document.querySelector(".box").innerHTML
+'\n            Hey I am a box\n        '
+document.querySelector(".container").nodeName
+'DIV'
+document.querySelector(".container").tagName
+'DIV'
+document.querySelector(".container").textContent
+'\n        \n            Hey I am a box\n        \n    '
+document.querySelector(".container").hidden
+false
+document.querySelector(".container").hidden = true
+true
+document.querySelector(".container").hidden = false
+false
+*/
